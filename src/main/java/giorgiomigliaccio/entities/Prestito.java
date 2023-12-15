166d3e0e7955705c59a5381b2e4a1c3e6c5e8296
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2023-12-15 11:35:44 +0100

    Add class Prestito

---
+++ src/main/java/giorgiomigliaccio/entities/Prestito.java
@@ -0,0 +1,83 @@
+package giorgiomigliaccio.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.time.LocalDate;
+@Entity
+public class Prestito {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @ManyToOne
+    private Utente utente;
+
+    @ManyToOne
+    private CatalogoBibliotecario elementoPrestato;
+
+    private LocalDate dataInizioPrestito;
+    private LocalDate dataRestituzionePrevista;
+    private LocalDate dataRestituzioneEffettiva;
+
+    public Prestito(Utente utente, CatalogoBibliotecario elementoPrestato, LocalDate dataInizioPrestito,LocalDate dataRestituzionePrevista,LocalDate dataRestituzioneEffettiva ) {
+        this.utente = utente;
+        this.elementoPrestato = elementoPrestato;
+        this.dataInizioPrestito = dataInizioPrestito;
+        this.dataRestituzionePrevista = dataRestituzionePrevista;
+        this.dataRestituzioneEffettiva = dataRestituzioneEffettiva;
+    }
+
+    public Utente getUtente() {
+        return utente;
+    }
+
+    public void setUtente(Utente utente) {
+        this.utente = utente;
+    }
+
+    public CatalogoBibliotecario getElementoPrestato() {
+        return elementoPrestato;
+    }
+
+    public void setElementoPrestato(CatalogoBibliotecario elementoPrestato) {
+        this.elementoPrestato = elementoPrestato;
+    }
+
+    public LocalDate getDataInizioPrestito() {
+        return dataInizioPrestito;
+    }
+
+    public void setDataInizioPrestito(LocalDate dataInizioPrestito) {
+        this.dataInizioPrestito = dataInizioPrestito;
+    }
+
+    public LocalDate getDataRestituzionePrevista() {
+        return dataRestituzionePrevista;
+    }
+
+    public void setDataRestituzionePrevista(LocalDate dataRestituzionePrevista) {
+        this.dataRestituzionePrevista = dataRestituzionePrevista;
+    }
+
+    public LocalDate getDataRestituzioneEffettiva() {
+        return dataRestituzioneEffettiva;
+    }
+
+    public void setDataRestituzioneEffettiva(LocalDate dataRestituzioneEffettiva) {
+        this.dataRestituzioneEffettiva = dataRestituzioneEffettiva;
+    }
+
+    @Override
+    public String toString() {
+        return "Prestito{" +
+                "id=" + id +
+                ", utente=" + utente +
+                ", elementoPrestato=" + elementoPrestato +
+                ", dataInizioPrestito=" + dataInizioPrestito +
+                ", dataRestituzionePrevista=" + dataRestituzionePrevista +
+                ", dataRestituzioneEffettiva=" + dataRestituzioneEffettiva +
+                '}';
+    }
+}
